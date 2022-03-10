577d6f3ae8af2cc7b245e8e2b8b7e7c6752283ca
Simon <semen.mokrov@gmail.com>
2022-03-11 00:33:49 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int a,b,c,d,e,f,g,h;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h;
+
+    if (a + c + e + g != b + d + f + h) {
+        cout << "IMPOSSIBLE";
+    }
+    else {
+
+    }
+
+    return 0;
+}
